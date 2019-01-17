5ff09d46c54496b84235fae3e4f9c3d329cb1cf9
Smbat141 <smbatpalpyan@gmail.com>
2019-01-17 18:16:40 +0400

    Blog2.1

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,51 +1,53 @@
+
 @extends('admin.layouts.app_admin')
 
 @section('content')
     <div class="container">
-        @component('admin.category.index')
-            @slot('title')Category List @endslot
-            @slot('parent')Main@endslot
-            @slot('active') List @endslot
+        @component('admin.components.breadcrumb')
+            @slot('title') Список категорий @endslot
+            @slot('parent') Главная @endslot
+            @slot('active') Категории @endslot
         @endcomponent
-    </div>
-    <hr>
 
-    <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right">
-        <i class="fa fa-plus-square-o">Create category</i>
-    </a>
-    <table class="table table-striped">
-        <thead>
-        <th>Name</th>
-        <th>Publications</th>
-        <th class="text-right">Actions</th>
-        </thead>
-        <tbody>
+        <hr>
+
+        <a href="{{route('admin.category.index')}}" class="btn btn-primary pull-right">
+            <i class="fa fa-plus-square-o"></i> Создать категорию
+        </a>
+
+        <table class="table table-striped">
+            <thead>
+            <th>Name</th>
+            <th>Puplic</th>
+            <th class="text-right">Оperation</th>
+            </thead>
+            <tbody>
             @forelse($categories as $category)
                 <tr>
-                    <th>{{$category->title}}</th>
-                    <th>{{$category->published}}</th>
+                    <td>{{$category->title}}</td>
+                    <td>{{$category->published}}</td>
                     <td>
-                        <a href="{{route('admin.category.edit',['id' => $category->id])}}">
-                            <i class="fa fa-edit"></i>
-                        </a>
+                        <a href="{{route('admin.category.edit', ['id'=>$category->id])}}"><i class="fa fa-edit"></i></a>
                     </td>
                 </tr>
                 @empty
-                    <tr>
-                        <td colspan="3" class="text-center">
-                            <h2>No data</h2>
-                        </td>
-                    </tr>
+                <tr>
+                    <td colspan="3" class="text-center"><h2>Данные отсутствуют</h2></td>
+                </tr>
             @endforelse
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="3">
-                    <ul class="pagination pull-right">
-                        {{$categories->links()}}
-                    </ul>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="3">
+                        <ul class="pagination pull-right">
+                            {{$categories->links()}}
+                        </ul>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+
+
+    </div>
+
 @endsection
